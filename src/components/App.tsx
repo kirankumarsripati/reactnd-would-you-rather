2fdfc0d7a863c89314b8a974f735ced9bf81fa2a
Kirankumar Sripati <krnsripati@gmail.com>
2020-06-04 14:49:58 +0530

    feat: add header

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from './Header';
+
+const App = () => <Header />
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Header = () => (
+  <Navbar bg="light" expand="md">
+    <Navbar.Brand href="/">Would You Rather</Navbar.Brand>
+    <Navbar.Toggle aria-controls="main-nav" />
+    <Navbar.Collapse id="main-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/add">Add Question</Nav.Link>
+        <Nav.Link href="/leaderboard">Leader Board</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
+
+export default Header;

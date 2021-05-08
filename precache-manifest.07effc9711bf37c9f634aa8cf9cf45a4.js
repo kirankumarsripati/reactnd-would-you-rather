ab5a3833cc801c3818d4958e0cbc2da2b9b65d8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-08 09:25:49 +0000

    Deploy kirankumarsripati/reactnd-would-you-rather to github.com/kirankumarsripati/reactnd-would-you-rather.git:gh-pages

---
+++ precache-manifest.07effc9711bf37c9f634aa8cf9cf45a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff74b2df9f5a65f2e35daa0054c5b77",
+    "url": "/reactnd-would-you-rather/index.html"
+  },
+  {
+    "revision": "9bb206015d48f06e1822",
+    "url": "/reactnd-would-you-rather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b4f0ce14ecd6394cc47d",
+    "url": "/reactnd-would-you-rather/static/css/main.a5669e9a.chunk.css"
+  },
+  {
+    "revision": "9bb206015d48f06e1822",
+    "url": "/reactnd-would-you-rather/static/js/2.614d1002.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactnd-would-you-rather/static/js/2.614d1002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f0ce14ecd6394cc47d",
+    "url": "/reactnd-would-you-rather/static/js/main.56149716.chunk.js"
+  },
+  {
+    "revision": "37a077f7036b7ccb5081",
+    "url": "/reactnd-would-you-rather/static/js/runtime-main.e5d53db9.js"
+  }
+]);
